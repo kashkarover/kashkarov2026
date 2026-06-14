8e383479f3be0ea84621d0d2c6cb99ab9657b6fc
Ravil <112270564+kashkarover@users.noreply.github.com>
2026-06-14 12:18:26 +0500

    Delete python/Pokolenie_Python_Prodvinutiy/task10.5.3 directory

--- python/Pokolenie_Python_Prodvinutiy/task10.5.3/solution.py
+++
@@ -1,7 +0,0 @@
-def is_access_allowed(ip_addres, mode, ip_access_lists):
-    if mode == 1 and ip_addres not in ip_access_lists['black list']:
-        return 'ДА'
-    elif mode == 2 and ip_addres in ip_access_lists['white list']:
-        return 'ДА'
-    else:
-        return 'НЕТ'
